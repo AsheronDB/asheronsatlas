af2c7156632369e850f4364c24ed5fb51ba4a408
Jesse L.K. Overton <jesse@ovrtn.com>
2023-09-15 02:08:15 -0400

    fix DerethMap

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,17 @@
 <template>
   <div class="h-screen" id="atlas">
-
-    <DerethMap />
- 
+    <ClientOnly>
+      <DerethMap />
+    </ClientOnly>
   </div>
 </template>
 
 <script setup>
-
 import DerethMap from "@/components/Atlas/Dereth/Map";
 
 definePageMeta({
   layout: "fill",
 });
-
 </script>
 
-<style scoped>
-
-
-
-
-</style>
+<style scoped></style>
